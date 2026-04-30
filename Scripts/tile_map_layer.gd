e553a66b3f262e259d7975971433f976b0803a71
Titus-345 <titusstauffer@gmail.com>
2026-04-30 12:30:17 -0500

    working dictionary tracker

--- Scripts/tile_map_layer.gd
+++ Scripts/tile_map_layer.gd
@@ -17,23 +17,30 @@ func _ready() -> void:
 	#DRAW THE BORDER OF THE GAME
 	drawTheTetrisBorder()
 
-var shapesMade = {
-	0: [],
-	1: []
+var shapeInstanceCount = 0
+var shapeBodyTracker: Dictionary[int, Array] = {
 }
 
-var iShape: Array[Vector2] = [Vector2(0,0), Vector2(1,0), Vector2(2,0)]
-var oShape: Array[Vector2] = [Vector2(0,0), Vector2(1,0), Vector2(0,1), Vector2(1,1)]
-
+const iShape: Array[Vector2] = [Vector2(0,0), Vector2(1,0), Vector2(2,0)]
+const oShape: Array[Vector2] = [Vector2(0,0), Vector2(1,0), Vector2(0,1), Vector2(1,1)]
+const tShape: Array[Vector2] = [Vector2(0,0), Vector2(1,0), Vector2(2,0), Vector2(1,1)]
+const jShape: Array[Vector2] = [Vector2(1,0), Vector2(1,1), Vector2(1,2), Vector2(0,2)]
+const lShape: Array[Vector2] = [Vector2(0,0), Vector2(0,1), Vector2(0,2), Vector2(1,2)]
+const sShape: Array[Vector2] = [Vector2(0,1), Vector2(1,1), Vector2(1,0), Vector2(2,0)]
+const zShape: Array[Vector2] = [Vector2(0,1), Vector2(1,1), Vector2(1,1), Vector2(2,1)]
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	makeTetromino(iShape, Vector2(0,0))
-	
+	if shapeInstanceCount <= 2:
+		makeTetromino(sShape, Vector2(0,-4))
+		makeTetromino(jShape, Vector2(0,0))
+		makeTetromino(tShape, Vector2(0,4))
+		print(shapeBodyTracker[2])
 	
 	
 func makeTetromino(shape: Array, location: Vector2, color: String = "red" ):
 	for n in shape.size():
-		set_cell(shape[n], 5, Vector2(0,0))
-		print(n)
+		set_cell(Vector2(shape[n].x+location.x, shape[n].y+location.y), 5, Vector2(0,0))
+	shapeBodyTracker[shapeInstanceCount] = shape
+	shapeInstanceCount += 1
